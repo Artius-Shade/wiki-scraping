8e50ccc5839058bf8643cfca941cade6bbfaebf0
Artius Shade <artius.shade@mailfence.com>
2022-03-04 17:09:54 +0530

    Create wiki.py

---
+++ wiki.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def short_def(search_):
+    '''Scrapes wikipedia to return a short defeniton of string as argument.'''
+    source = requests.get(f'https://en.wikipedia.org/wiki/{search_}').text
+
+    soup = BeautifulSoup(source, 'lxml')
+    # Narrowing the way down to item of intrest (first paragraph of wikipedia page) in the html text from internet
+    intrs = soup.body
+    intrs = intrs.find('div', class_='mw-body')
+    intrs = intrs.find('div', class_='vector-body')
+    intrs_list = intrs.find_all('p', class_='')
+    # By observations it was found that only that the pargraph tag containing nested bold tag is the item of intrest.
+    # but that may not be true always as I only tested the program on few pages.
+    for item in intrs_list:
+        if item.b:
+            intrs = item
+            break
+    heading = intrs.b.text
+    # When the search is not found the server responds with "Other reasons this message may be displayed:",
+    # which is quite ambiguos of an 'not found message' so, this function returns this not found which gives better
+    # idea of what went wrong.
+    if heading.startswith('Other reasons this'):
+        return "Sorry, your search can't be found. Try again with different words."
+
+    if intrs.text.endswith('may refer to:\n'):
+        return "Sorry, your search can't be found. Try again with different words."
+
+    return f">>>{heading.title()}\n{intrs.text}"
+
+
+def wiki(search_):
+    '''A modified version of short_def, which retuns the defeniton of argument as well as 
+    defenitons of all the keywords found within the defeniton of argument.'''
+    source = requests.get(f'https://en.wikipedia.org/wiki/{search_}').text
+
+    soup = BeautifulSoup(source, 'lxml')
+    intrs = soup.body
+    intrs = intrs.find('div', class_='mw-body')
+    intrs = intrs.find('div', class_='vector-body')
+    intrs_list = intrs.find_all('p', class_='')
+    for item in intrs_list:
+        if item.b:
+            intrs = item
+            break
+    sl = [search_]
+    # On some pages there can be a tags which does not have a title key, and can causes the function to
+    # return an error. This for loop filters out the tags without exceptions.
+    for x in intrs.find_all('a'):
+        try:
+            sl.append(x['title'])
+        except KeyError:
+            continue
+    for s in sl:
+        yield short_def(s)
